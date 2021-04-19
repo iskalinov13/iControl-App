bfe3a251a6c8fc17ef978d16728ad03e1fc1a719
Farukh <170107160@stu.sdu.edu.kz>
2021-04-19 18:29:32 +0600

    Initial Commit

---
+++ iControlAppTests/iControlAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  iControlAppTests.swift
+//  iControlAppTests
+//
+//  Created by Farukh Iskalinov on 19.04.21.
+//
+
+import XCTest
+@testable import iControlApp
+
+class iControlAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
